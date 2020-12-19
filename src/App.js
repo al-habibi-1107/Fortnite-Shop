5508a896269fc2558459989110c343bed5a2224e
Kamil <kamilanwar2001@gmail.com>
2020-12-20 00:04:48 +0400

    Landing page UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import Navbar from './components/Navbar';
+import Home from './components/Home'
 import './App.css';
 
 function App() {
   return (
+    <>
     <Navbar/>
+    <Home/>
+    </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './Home.css';
+
+
+function Home(){
+
+return (
+    <>
+    <img src="https://static.giga.de/wp-content/uploads/2020/04/fortniteseason2kapitel2.jpg" alt='bg'/>
+    <div className="btn">
+    <button>Shop Now</button>
+    </div>
+    </>
+);
+
+}
+
+export default Home;
